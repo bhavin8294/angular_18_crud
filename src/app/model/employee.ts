fa1121c61976b5711959f2b7215cfe708a9356b5
Bhavin Surati <bhavin.surati@vsiinfotech.com>
2025-12-23 16:14:17 +0530

    Added the Html for Employee Form

---
+++ src/app/model/employee.ts
@@ -0,0 +1,20 @@
+export class employeeModal {
+    empId: number;
+    name: string;
+    emailId: string;
+    city: string;
+    state: string;
+    contactNo: string;
+    address: string;
+
+    constructor() {
+        this.address = '';
+        this.city = '';
+        this.contactNo = '';
+        this.address = '';
+        this.emailId = '';
+        this.empId = 0;
+        this.state = '';
+        this.name = '';
+    }
+}
